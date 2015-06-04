649b5e8af19998a0cc7d77d37bd98e2897ee5966
Occitech <contact@occitech.fr>
2015-06-04 12:57:43 +0200

    Moar

--- app.js
+++ app.js
@@ -3,30 +3,30 @@ import Bacon from 'baconjs';
 
 $.fn.asEventStream = Bacon.$.asEventStream;
 
+const redmineURL = 'http://projets.occitech.fr';
+
 const projectSelector = $('#projet');
 const versionSelector = $('#version');
+const issuesContainer = $('#results');
 
-let redmineApiKey = $('[name=redmine]').asEventStream('change')
-	.map('.target.value')
-	.toProperty(localStorage.getItem('redmineApiKey'));
+function configuration(field, localStorageKey) {
+	let config = field.asEventStream('change')
+		.map('.target.value')
+		.toProperty(localStorage.getItem(localStorageKey));
 
-redmineApiKey.onValue((key) => localStorage.setItem("redmineApiKey", key));
-redmineApiKey.onValue((key) => $('[name=redmine]').val(key));
+	config.onValue((key) => localStorage.setItem(localStorageKey, key));
+	config.onValue((key) => field.val(key));
+	return config;
+}
 
-let togglApiKey = $('[name=toggl]')
-	.asEventStream('change')
-	.map('.target.value')
-	.toProperty(localStorage.getItem('togglApiKey'));
-togglApiKey.onValue((key) => localStorage.setItem("togglApiKey", key));
-togglApiKey.onValue((key) => $('[name=toggl]').val(key));
+let redmineApiKey = configuration($('[name=redmine]'), 'redmineApiKey');
+let togglApiKey = configuration($('[name=toggl]'), 'togglApiKey');
 
 function redmineAPICall(apiKey, resource, params) {
-	const url = 'http://projets.occitech.fr';
-
 	params = params || { };
 	params.key = apiKey;
 
-	return $.get(url + resource, params);
+	return $.get(redmineURL + resource, params);
 }
 
 function togglReportsAPICall(apiKey, resource, params) {
@@ -41,7 +41,7 @@ function togglReportsAPICall(apiKey, resource, params) {
 		data: params,
 		headers: {
 			// see http://stackoverflow.com/a/5507289
-			"Authorization": "Basic " + btoa(apiKey + ":" + 'api_token')
+			"Authorization": "Basic " + btoa(`${apiKey}:api_token`)
 		},
 	});
 }
@@ -52,7 +52,7 @@ function notEmpty(val) {
 
 let projets = redmineApiKey
 	.filter(notEmpty)
-	.flatMap((apiKey) => Bacon.fromPromise(redmineAPICall(apiKey, '/projects.json', { limit: 100 })))
+	.flatMapLatest((apiKey) => Bacon.fromPromise(redmineAPICall(apiKey, '/projects.json', { limit: 100 })))
 	.map('.projects');
 
 function updateSelect(elt, valueKey, labelKey) {
@@ -61,25 +61,82 @@ function updateSelect(elt, valueKey, labelKey) {
 	return function(source) {
 		elt.find('option').remove();
 		elt.append('<option value="">----</option>');
-		source.map((item) => elt.append('<option value="' + item[valueKey] + '">' + item[labelKey] + '</option>'))
+		source.map((item) => elt.append(`<option value="${item[valueKey]}">${item[labelKey]}</option>`))
 	}
 }
 
 projets.onValue(updateSelect(projectSelector));
 
-let versions = projectSelector.asEventStream('change')
+let issues = projectSelector.asEventStream('change')
 	.map((e) => $(e.target).val())
-	.filter(notEmpty)
 	.combine(redmineApiKey, function(projectId, apiKey) {
 		return { projectId: projectId, apiKey: apiKey };
 	})
-	.flatMap((data) => Bacon.fromPromise(redmineAPICall(data.apiKey, '/projects/' + data.projectId + '/versions.json', { limit: 100 })))
-	.map('.versions');
+	.flatMapLatest((data) => Bacon.fromPromise(redmineAPICall(data.apiKey, '/issues.json', {
+		limit: 100,
+		project_id: data.projectId
+	})))
+	.map('.issues');
+
+function groupByVersion(issues) {
+	return issues.reduce((groupedIssues, issue) => {
+		const version = issue.fixed_version || { id: 'none', name: 'Sans version' };
+
+		if (!groupedIssues.hasOwnProperty(version.id)) {
+			groupedIssues[version.id] = {
+				name: version.name,
+				issues: []
+			};
+		}
+
+		groupedIssues[version.id].issues.push(issue);
+		return groupedIssues;
+	}, {});
+}
+
+let issuesByVersion = issues
+	.map(groupByVersion);
+
+function issueItem(issue) {
+	let url = `${redmineURL}/issues/${issue.id}`;
+	return `<li><a href="${url}" target="_blank">#${issue.id}</a> - ${issue.subject}</li>`
+}
+
+function updateIssues(issuesByVersion) {
+	issuesContainer.html('');
+
+	for (let versionId in issuesByVersion) {
+		let version = issuesByVersion[versionId];
+		let html = `
+			<div class="version">
+				<h2>${version.name}</h2>
+				<ul>
+					${version.issues.map(issueItem).join("\n")}
+				<ul>
+			</div>
+		`;
+		issuesContainer.append(html);
+	};
+}
+
+issuesByVersion.onValue(updateIssues);
+
+let versions = issuesByVersion
+	.map((issuesByVersion) => {
+		let versions = [];
+		for (let versionId in issuesByVersion) {
+			versions.push({
+				id: issuesByVersion[versionId].id,
+				name: issuesByVersion[versionId].name
+			});
+		}
+		return versions;
+	});
 
 versions.onValue(updateSelect(versionSelector));
 
-let entries = togglApiKey
-	.filter(notEmpty)
-	.flatMap((apiKey) => Bacon.fromPromise(togglReportsAPICall(apiKey, '/details')))
-	.log('toggl');
+// let entries = togglApiKey
+// 	.filter(notEmpty)
+// 	.flatMapLatest((apiKey) => Bacon.fromPromise(togglReportsAPICall(apiKey, '/details')))
+// 	.log('toggl');
 
